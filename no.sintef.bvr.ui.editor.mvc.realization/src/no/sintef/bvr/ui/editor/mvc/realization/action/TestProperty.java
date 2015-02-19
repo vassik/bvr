52187676afe2a8072582d8a8f1ad2f59eafeca2d
vassik <Anatoly.Vasilevskiy@sintef.no>
2015-02-19 17:13:56 +0100

    test property menu

---
+++ no.sintef.bvr.ui.editor.mvc.realization/src/no/sintef/bvr/ui/editor/mvc/realization/action/TestProperty.java
@@ -0,0 +1,15 @@
+package no.sintef.bvr.ui.editor.mvc.realization.action;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class TestProperty extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("");
+		return null;
+	}
+
+}
